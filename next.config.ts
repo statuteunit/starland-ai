8c1c17c4d9e511692aedf04c342575d2c2909fe8
xiangyuan.su <xiangyuan.su@xgo.one>
2026-02-09 17:52:17 +0800

    chore: 移除Next.js配置中的basePath和assetPrefix
    不再需要为特定仓库设置基础路径和资源前缀，简化部署配置。

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
-const repo = "starland-ai";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}`,
 };
 
 export default nextConfig;
